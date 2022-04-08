c77434a472cf54b400fe3c56be72c18287626bdd
Eitan Shmuel Corenblum <carl158@gmail.com>
2022-04-08 14:32:11 -0400

    finish crud

--- api/__init__.py
+++ api/__init__.py
@@ -8,10 +8,16 @@ from config import Config
 from api.models.user import User
 from api.models.profile import Profile
 from api.models.cat import Cat
+from api.models.toy import Toy
+
 
 
 # ============ Import Views ============
 from api.views.auth import auth
+from api.views.cats import cats
+from api.views.toys import toys
+
+
 
 cors = CORS()
 migrate = Migrate() 
@@ -27,6 +33,9 @@ def create_app(config):
 
   # ============ Register Blueprints ============
   app.register_blueprint(auth, url_prefix='/api/auth') 
+  app.register_blueprint(cats, url_prefix='/api/cats') 
+  app.register_blueprint(toys, url_prefix='/api/toys') 
+
 
 
   return app
